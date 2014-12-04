b15a4ddf0aea93372c8670ce5c1171871d556d2c
young <young.mwy@gmail.com>
2014-12-04 22:11:51 +0800

    print log before isntalling

--- install.sh
+++ install.sh
@@ -4,8 +4,10 @@ if [[ ! -d ~/.tmux ]]; then
     mkdir ~/.tmux
 fi
 
+echo "1. install ~/.tmux.conf"
 cp ./tmux.conf ~/.tmux.conf
-echo "install ~/.tmux.conf"
 
+echo "2. install ~/.tmux/layout"
 cp ./layout ~/.tmux/layout
-echo "install ~/.tmux/layout"
+
+echo "install OK!"
